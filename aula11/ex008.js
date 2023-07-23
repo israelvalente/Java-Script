8512006b41f632458d29377cf744753cc043a08b
Filipe Alves <131316142+israelvalente@users.noreply.github.com>
2023-07-23 20:58:50 -0300

    some updates

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 40
+console.log(`The velocity of your car is ${vel}km/h`)
+if (vel > 60){ //condição simples.
+    console.log(`Você ultrapassou a velocidade máxima. Multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
