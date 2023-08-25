8cb13d424dbeab49eb5c4e3170d6b44e14e4078f
Filipe Alves <131316142+israelvalente@users.noreply.github.com>
2023-08-25 15:00:42 -0300

    updates

---
+++ youtube/javascript.js
@@ -0,0 +1,9 @@
+let name = "Filipe"
+let age = 37
+let student = true;
+
+document.getElementById("p1").innerHTML = "Hello " + name + "!"
+
+document.getElementById("p2").innerHTML = "You are " + age + " years old."
+
+document.getElementById("p3").innerHTML = "Someone said you are studying and I know that it is " + true
